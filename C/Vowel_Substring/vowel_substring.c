29c36d1ae88902a5f584e0d31099e8654f2113ed
YounesMoukhlij <123767564+YounesMoukhlij@users.noreply.github.com>
2024-06-11 18:14:41 +0100

    new

--- C/Vowel_Substring/vowel_substring.c
+++ C/Vowel_Substring/vowel_substring.c
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:10:37 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/06/11 16:51:36 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/06/11 17:37:48 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,9 @@
 #include <string.h>
 #include <stdio.h>
 
-int	not_found(char *s)
-{
-	int	i;
-	int	counter;
-
-	i = 0x0;
-	counter = 0x0;	
-	while (s[i])
-	{
-		if (s[i] == 'a' || s[i] == 'e' || s[i] == 'o' || s[i] == 'i' || s[i] == 'u')
-			counter++;
-		i++;
-	}
-	if (!counter)
-		return (0x0);
-	return (counter);
-}
-
 char	*vowel_substring(char *s, int num)
 {
-	int		i;
-	char	*str;
-	int		counter;
-
-	(void) str;
-	counter = 0x0;
-	if (!not_found(s))
-		return ("NOT FOUND");
-	while (s[i])
-	{
-		if (s[i] == 'a' || s[i] == 'e' || s[i] == 'o' || s[i] == 'i' || s[i] == 'u')
-		{
-			while (s[i])
-			{
-				
-			}
-		}
-		i++;
-
-
-
-		
-	}
-	// str = malloc(num + 0x1);
-	// if (!str)
-	// 	return NULL;
-	str = strdup("");
-	return str;
+	
 }
 
 int main(int ac, char *av[])
@@ -73,9 +28,6 @@ int main(int ac, char *av[])
 	str = vowel_substring(av[0x1], atoi(av[0x2]));
 	printf("the Word you entred = [%s]\n", av[0x1]);
 	printf("result (Substred str) = [%s]\n", str);
-	// free (str);
 	return (0x0);
 }
 
-erioipkl
-4
